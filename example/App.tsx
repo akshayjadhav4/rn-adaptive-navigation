0a1bc0b7a9b685e5ac1a2259b700592503c56c5b
Akshay Jadhav <akshayjadhav461999@gmail.com>
2025-01-20 10:46:51 +0530

    cleanup

--- example/App.tsx
+++ example/App.tsx
@@ -1,73 +1,21 @@
-import { useEvent } from 'expo';
-import AdaptiveNavigation, { AdaptiveNavigationView } from 'adaptive-navigation';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function App() {
-  const onChangePayload = useEvent(AdaptiveNavigation, 'onChange');
-
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{AdaptiveNavigation.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{AdaptiveNavigation.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await AdaptiveNavigation.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <AdaptiveNavigationView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
-      </ScrollView>
+      <Text style={styles.title}>Adaptive Navigation</Text>
     </SafeAreaView>
   );
 }
 
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
-    </View>
-  );
-}
-
-const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-  view: {
-    flex: 1,
-    height: 200,
+  title: {
+    fontSize: 28,
+    fontWeight: 800,
   },
-};
+});
