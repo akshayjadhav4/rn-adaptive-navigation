86988c8de9ab44c344801eb5dd79ed065a1350a9
Akshay Jadhav <akshayjadhav461999@gmail.com>
2025-01-21 18:52:24 +0530

    basic bottom navigation view and screen render setup

--- android/src/main/java/expo/modules/adaptivenavigation/AdaptiveNavigationView.kt
+++ android/src/main/java/expo/modules/adaptivenavigation/AdaptiveNavigationView.kt
@@ -1,30 +1,76 @@
 package expo.modules.adaptivenavigation
 
 import android.content.Context
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.View
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
 
-class AdaptiveNavigationView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
-  // Creates and initializes an event dispatcher for the `onLoad` event.
-  // The name of the event is inferred from the value and needs to match the event name defined in the module.
-  private val onLoad by EventDispatcher()
+class AdaptiveNavigationView(context: Context, appContext: AppContext) :
+    ExpoView(context, appContext) {
 
-  // Defines a WebView that will be used as the root subview.
-  internal val webView = WebView(context).apply {
-    layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-    webViewClient = object : WebViewClient() {
-      override fun onPageFinished(view: WebView, url: String) {
-        // Sends an event to JavaScript. Triggers a callback defined on the view component in JavaScript.
-        onLoad(mapOf("url" to url))
-      }
+    private val bottomNavigation = BottomNavigationView(context)
+    private val frameLayout = FrameLayout(context)
+    internal val navigation = bottomNavigation.apply {
+
     }
-  }
 
-  init {
-    // Adds the WebView to the view hierarchy.
-    addView(webView)
-  }
+    init {
+        orientation = VERTICAL
+
+        addView(
+            frameLayout, LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                0,
+            ).apply {
+                weight = 1f
+            } // expand to fill any remaining vertical space after bottomNavigation have been laid out
+        )
+        // it won’t preserve its state during configuration changes
+        frameLayout.isSaveEnabled = false
+
+//        layoutHolder.setBackgroundColor(Color.parseColor("#F3B63A"))
+//        navigation.setBackgroundColor(Color.parseColor("#FF0000"))
+
+        addView(navigation, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT))
+
+    }
+
+    override fun addView(child: View, index: Int) {
+        when (child) {
+            navigation, frameLayout -> {
+
+                super.addView(child, index)
+            }
+
+            else -> {
+                // Create a new container for this child
+                val sceneContainer = createSceneContainer()
+
+                // TEMP: to test Component is rendering or not
+                sceneContainer.visibility = if (index == 0) View.VISIBLE else View.INVISIBLE
+                sceneContainer.isEnabled = if (index == 0) true else false
+
+                // Add container to layoutHolder
+                frameLayout.addView(sceneContainer)
+
+                // Add the child to its container
+                sceneContainer.addView(child)
+            }
+        }
+    }
+
+    private fun createSceneContainer(): FrameLayout {
+        val sceneContainer = FrameLayout(context).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+            visibility = INVISIBLE
+            isEnabled = false
+        }
+        return sceneContainer
+    }
+
 }
